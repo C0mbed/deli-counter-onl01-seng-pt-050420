3618bccfb97f37b2cb68691595ce9feca5e61eef
Matt Farncombe <matt@farncombe.co>
2020-05-07 13:12:08 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -9,7 +9,9 @@ def line(katz_deli)
     puts "The line is currently empty."
   else
     katz_deli.each do |person|
-      
+      current = "#{counter}. #{person}"
+      format_line << current
+      counter++
     end
     puts "The line is currently: #{katz_deli}"
     
