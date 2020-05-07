007289eb71742bad9d9a4ac4884a06e0b0c26454
Matt Farncombe <matt@farncombe.co>
2020-05-07 13:03:05 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -8,7 +8,7 @@ def line(katz_deli)
   if katz_deli.length == 0
     puts "The line is currently empty."
   else
-    puts "The line is currently: #{formatted_serving}"
+    puts "The line is currently: #{katz_deli}"
   end
 end
 
