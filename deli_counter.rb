19284291d77d552cd7af661de970a2bb86839ec3
Matt Farncombe <matt@farncombe.co>
2020-05-07 12:47:59 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -7,13 +7,6 @@ def line(katz_deli)
   if katz_deli.length == 0
     puts "The line is currently empty."
   else
-    counter = 1
-    katz_deli.each do |customer|
-      format_line << "#{counter}. #{customer}"
-      counter++
-    end
-    now_serving = format_line.join(" ")
-    puts "The line is currently: #{now_serving}"
   end
 end
 
@@ -23,4 +16,4 @@ def take_a_number(katz_deli, name)
   puts "Welcome, #{name}.  You are number #{current_place} in line."
   return katz_deli
 end
-end
+
